0fac22a6f4cf7828b5f934f818b3506e783f74c1
komine <komine@m-lic.co.jp>
2023-01-12 12:10:42 +0900

    utils追加

---
+++ asp_env_test_pub/asp_env_test_pub/Utils/TestUtil.cs
@@ -0,0 +1,15 @@
+﻿namespace asp_env_test_pub.Utils
+{
+    public class TestUtil
+    {
+        public string JoinStr(string strA, string strB)
+        {
+            return strA + strB;
+        }
+
+        public int AddInt(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
